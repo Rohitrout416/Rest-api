72f331f18d38cb7441e690528147d81da7c16274
Rohitrout416 <79717560+Rohitrout416@users.noreply.github.com>
2022-07-20 23:25:00 +0530

    done with mongodb now

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 import { v4 as uuidV4 } from 'uuid';
 import userDetails from "../models/userModel.js"
+import mongoose from 'mongoose';
 
 
 let users = [
@@ -34,12 +35,28 @@ export const display_user = async(req, res)=>{
     res.status(200).json(users);
 }
 
-export const get_user = (req, res)=>{
-    const userId = req.params.id;
+//get one user
+// export const get_user = (req, res)=>{
+//     const userId = req.params.id;
 
-    const user = users.find(user => user.id === userId)
+//     const user = users.find(user => user.id === userId)
 
-    res.json(user)
+//     res.json(user)
+// }
+
+
+//get one user with mongodb
+export const get_user = async(req, res)=>{
+    const { id: userId } = req.params;
+    
+    if(!mongoose.Types.ObjectId.isValid(userId))
+    {
+        return res.status(404).json({error:"No such user!"});
+    }
+    
+    const user = await userDetails.findById(userId);
+    
+    res.status(200).json(user);
 }
 
 export const add_user = async(req, res)=>{
@@ -49,7 +66,7 @@ export const add_user = async(req, res)=>{
         age,
         id: uuidV4()
     })
-
+    
     try {
         const userdetails = await userDetails.create({name, age, Aadhar});
         res.status(200).json(userdetails);
@@ -59,26 +76,69 @@ export const add_user = async(req, res)=>{
     // res.json(users)
 }
 
-export const delete_user = (req, res)=>{
-    const userId = req.params.id;
-
-    users = users.filter(user => user.id !== userId);
-    res.json(users);
-}
-
-export const update_user = (req, res)=>{
-    const userId = req.params.id;
-    const {name, age} = req.body;
-
-    users = users.map((user)=>{
-        if (user.id === userId) {
-            return{
-                name,
-                age,
-                id: userId
-            }
+//delete a user
+// export const delete_user = (req, res)=>{
+    //     const userId = req.params.id;
+    
+    //     users = users.filter(user => user.id !== userId);
+    //     res.json(users);
+    // }
+    
+    //delete a user with mongodb
+    export const delete_user = async(req, res)=>{
+        const {id: userId} = req.params;
+        
+        if(!mongoose.Types.ObjectId.isValid(userId))
+        {
+            return res.status(404).json({error:"No such user!"});
         }
-        return user;
-    })
-    res.json(users);
+        
+        const users = await userDetails.findOneAndDelete({_id: userId});
+        
+        if(!users)
+        {
+            return res.status(400).json({error:"No such error!"});
+        }
+        
+        res.status(200).json(users);
+        
+    }
+    
+    //update a user
+    // export const update_user = (req, res)=>{
+        //     const userId = req.params.id;
+        //     const {name, age} = req.body;
+        
+        //     users = users.map((user)=>{
+            //         if (user.id === userId) {
+                //             return{
+                    //                 name,
+                    //                 age,
+                    //                 id: userId
+                    //             }
+                    //         }
+                    //         return user;
+                    //     })
+                    //     res.json(users);
+                    // }
+                    
+                    
+//update a user with mongodb
+export const update_user = async(req, res)=>{
+    const { id: userId } = req.params;
+    
+    if(!mongoose.Types.ObjectId.isValid(userId))
+    {
+        return res.status(404).json({error:"No such user!"});
+    }
+
+    const user = await userDetails.findOneAndUpdate({_id: userId},{...req.body});
+
+    if(!user)
+    {
+        return res.status(400).json({error: "No such user!"});
+    }
+
+    res.status(200).json(user);
+
 }
